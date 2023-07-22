94c7f593fbf99cde22344d1a83ab512f5f2ff451
edwin martinez <edwmartinezg@outlook.com>
2023-07-21 21:59:07 -0500

    establecer comunicacion entre componentes.

--- BlazorMusicCatalog.Models/Song.cs
+++ BlazorMusicCatalog.Models/Song.cs
@@ -9,6 +9,7 @@ namespace BlazorMusicCatalog.Models
     public class Song
     {
         public int Id { get; set; }
+        public int Number { get; set; }
         public string Name { get; set; }
         public string Path { get; set; }
         public string Extension { get; set; }
